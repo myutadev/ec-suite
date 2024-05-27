7b34c12e80b66ce52d1a6affdb778f64b6830d05
myuta <myuta.dev@gmail.com>
2024-05-27 12:04:45 +0800

    change error handling logic

--- src/api/sp-api/fe/sg/writeProdCurPriceBySheetBatch.js
+++ src/api/sp-api/fe/sg/writeProdCurPriceBySheetBatch.js
@@ -25,49 +25,54 @@ const writeProdCurPriceBySheetBatch = async (
   let updateStartRow = readRow;
 
   for (let i = 0; i < asinArr.length; i += batchSize) {
-    console.log(`batch ${i} started`);
-    const priceInfoArr = [];
-    let updateEndRow = updateStartRow + batchSize - 1;
-    let updateRange = `${sheetName}!${updateStartCol}${updateStartRow}:${updateEndCol}${updateEndRow}`;
-
-    const batch = asinArr.slice(i, i + batchSize);
-
-    const curItemOffersObjArr = await getItemOffersBatch(batch);
-
-    const promises = curItemOffersObjArr.map(async (obj) => {
-      console.log("cur obj is", obj);
-      try {
-        console.log("obj is", obj);
-        return getAvailablePriceArr(obj, Object.keys(obj)[0]);
-      } catch (e) {
-        notifySlack(e);
-        return;
-      }
-    });
-
-    await Promise.allSettled(promises).then((results) =>
-      results.forEach((result) => {
-        if (result.status === "fulfilled") {
-          console.log("result.value", result.value);
-          priceInfoArr.push(result.value);
-        } else {
-          priceInfoArr.push([], [result.reason.message]);
-        }
-      })
-    );
-
     try {
-      await updateArrayDataToSheets(spreadsheetId, updateRange, priceInfoArr);
+      console.log(`batch ${i} started`);
+      const priceInfoArr = [];
+      let updateEndRow = updateStartRow + batchSize - 1;
+      let updateRange = `${sheetName}!${updateStartCol}${updateStartRow}:${updateEndCol}${updateEndRow}`;
+
+      const batch = asinArr.slice(i, i + batchSize);
+
+      const curItemOffersObjArr = await getItemOffersBatch(batch);
+
+      const promises = curItemOffersObjArr.map(async (obj) => {
+        console.log("cur obj is", obj);
+        try {
+          console.log("obj is", obj);
+          return getAvailablePriceArr(obj, Object.keys(obj)[0]);
+        } catch (e) {
+          notifySlack(e);
+          return;
+        }
+      });
+
+      await Promise.allSettled(promises).then((results) =>
+        results.forEach((result) => {
+          if (result.status === "fulfilled") {
+            console.log("result.value", result.value);
+            priceInfoArr.push(result.value);
+          } else {
+            priceInfoArr.push([], [result.reason.message]);
+          }
+        })
+      );
+
+      try {
+        await updateArrayDataToSheets(spreadsheetId, updateRange, priceInfoArr);
+      } catch (error) {
+        notifySlack(error);
+        continue;
+      }
+
+      // 10秒に1回のペースに制御 : だいたい7000にすると10秒に1回くらいになった
+      await new Promise((resolve) => setTimeout(resolve, 10000));
+
+      updateStartRow += batchSize;
+      console.log(`batch ${i} end`);
     } catch (error) {
       notifySlack(error);
-      throw error;
+      continue;
     }
-
-    // 10秒に1回のペースに制御 : だいたい7000にすると10秒に1回くらいになった
-    await new Promise((resolve) => setTimeout(resolve, 10000));
-
-    updateStartRow += batchSize;
-    console.log(`batch ${i} end`);
   }
 };
 
