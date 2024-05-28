3c2d8926e231c78e6b144f4ff031fddd8619be16
myuta <myuta.dev@gmail.com>
2024-05-28 09:01:40 +0800

    fix bug in notify slack.js

--- src/lib/notifySlack.js
+++ src/lib/notifySlack.js
@@ -1,13 +1,19 @@
+const axios = require("axios");
+require("dotenv").config();
+
 function notifySlack(error) {
   const SLACK_WEBHOOK_URL = process.env.SLACK_WEBHOOK_URL;
+  console.log(SLACK_WEBHOOK_URL);
   axios
     .post(SLACK_WEBHOOK_URL, {
       text: `An error occurred in EC-suite WORKER: ${error.message} \n stack trace:${error.stack}`,
     })
     .then(() => console.log("Notified Slack about the error."))
-    .catch(() => console.log("Error occurred while notifying Slack."));
+    .catch((e) => console.log("Error occurred while notifying Slack.", e));
 }
 
 module.exports = {
   notifySlack,
 };
+
+// notifySlack({ error: "test error" });
