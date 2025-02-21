be8fe420823c08a77c0b9267189bcb3b59b5872f
myuta <myuta.dev@gmail.com>
2025-02-21 22:49:22 +0900

    limit columns

--- src/api/sp-api/na/writeReportData.js
+++ src/api/sp-api/na/writeReportData.js
@@ -1,6 +1,7 @@
 const { appendArrayDataToSheets } = require("../../../lib/appendArrayDataToSheets.js");
 const { getReportData } = require("./getReportData.js");
 const { getStartOfYesterday, getEndOfYesterday } = require("../../../lib/getYesterday.js");
+const fs = require("fs");
 require("dotenv").config();
 
 const writeReportData = async (spreadsheetId, range, reportName, start, end, marketPlace) => {
@@ -17,8 +18,73 @@ const writeReportData = async (spreadsheetId, range, reportName, start, end, mar
     }
 
     const values = amazonData.map((obj) => {
-      const row = Object.values(obj);
-      row.unshift(startDate);
+      const row = [
+        startDate,
+        obj["snapshot-date"],
+        obj["sku"],
+        obj["fnsku"],
+        obj["asin"],
+        obj["product-name"],
+        obj["condition"],
+        obj["available"],
+        obj["pending-removal-quantity"],
+        obj["inv-age-0-to-90-days"],
+        obj["inv-age-91-to-180-days"],
+        obj["inv-age-181-to-270-days"],
+        obj["inv-age-271-to-365-days"],
+        obj["inv-age-365-plus-days"],
+        obj["currency"],
+        obj["qty-to-be-charged-ltsf-6-mo"],
+        obj["projected-ltsf-6-mo"],
+        obj["qty-to-be-charged-ltsf-12-mo"],
+        obj["estimated-ltsf-next-charge"],
+        obj["units-shipped-t7"],
+        obj["units-shipped-t30"],
+        obj["units-shipped-t60"],
+        obj["units-shipped-t90"],
+        obj["alert"],
+        obj["your-price"],
+        obj["sales-price"],
+        obj["lowest-price-new-plus-shipping"],
+        obj["lowest-price-used"],
+        obj["recommended-action"],
+        obj["healthy-inventory-level"],
+        obj["recommended-sales-price"],
+        obj["recommended-sale-duration-days"],
+        obj["recommended-removal-quantity"],
+        obj["estimated-cost-savings-of-recommended-actions"],
+        obj["sell-through"],
+        obj["item-volume"],
+        obj["volume-unit-measurement"],
+        obj["storage-type"],
+        obj["storage-volume"],
+        obj["marketplace"],
+        obj["product-group"],
+        obj["sales-rank"],
+        obj["days-of-supply"],
+        obj["estimated-excess-quantity"],
+        obj["weeks-of-cover-t30"],
+        obj["weeks-of-cover-t90"],
+        obj["featuredoffer-price"],
+        obj["sales-shipped-last-7-days"],
+        obj["sales-shipped-last-30-days"],
+        obj["sales-shipped-last-60-days"],
+        obj["sales-shipped-last-90-days"],
+        obj["inv-age-0-to-30-days"],
+        obj["inv-age-31-to-60-days"],
+        obj["inv-age-61-to-90-days"],
+        obj["inv-age-181-to-330-days"],
+        obj["inv-age-331-to-365-days"],
+        obj["estimated-storage-cost-next-month"],
+        obj["inbound-quantity"],
+        obj["inbound-working"],
+        obj["inbound-shipped"],
+        obj["inbound-received"],
+        obj["no-sale-last-6-months"],
+        obj["reserved-quantity"],
+        obj["unfulfillable-quantity"],
+        obj["historical-days-of-supply"],
+      ];
       return row;
     });
 
@@ -47,8 +113,8 @@ module.exports = {
 //   process.env.SPREADSHEET_ID,
 //   "InvReport_CA!A2:AB",
 //   "GET_FBA_INVENTORY_PLANNING_DATA",
-//   `2024-01-23T00:00:00-07:00`,
-//   `2024-01-23T23:59:59-07:00`,
+//   `2025-02-19T00:00:00-07:00`,
+//   `2025-02-19T23:59:59-07:00`,
 //   "CA"
 // );
 
